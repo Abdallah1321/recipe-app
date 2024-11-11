2c91fcd9551db1a99da3f8fd587a210c39c0e948
Abdallah1321 <abdallah.hussein.ibrahim@gmail.com>
2024-11-11 11:21:39 +0200

    Created footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="container main">
         <Hero />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <div className="footer container">
+      <div className="footer-section">
+        <p className="title">AbdallahsRecipes.com</p>
+        <p>
+          Be able to make any of your favourite foods and enjoy them with ease!
+        </p>
+        <p>&copy; 2024 | All Rights Reserved</p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Contact Me!</p>
+        <p>
+          abdallah.hussein.ibrahim@gmail.com
+        </p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Socials</p>
+        <p>
+          TikTok
+        </p>
+      </div>
+    </div>
+  );
+}
