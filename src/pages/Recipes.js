64d9e00b0b5907e1383e7fe41e97ded8877876d3
Abdallah1321 <abdallah.hussein.ibrahim@gmail.com>
2024-11-12 11:19:23 +0200

    Created search bar

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,30 @@
-export default function Recipes(){
-    return(
-        <div>
-            Recipes Page
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faS, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+export default function Recipes() {
+  const searches = ["Brownies", "Burger", "Cookies", "Tiramisu", "Hash Browns"];
+  return (
+    <div>
+      <div className="featured-searches section">
+        <h2>Featured Searches</h2>
+        <div className="featured-searches-container">
+          {searches.map((search, index) => (
+            <div
+              key={index}
+              style={{ animationDelay: index * 0.1 + "s" }}
+              className="search-item"
+            >
+              {search}
+            </div>
+          ))}
         </div>
-    )
+        <div className="search-box">
+          <input type="text" placeholder="Search... " />
+          <button className="btn">
+            <FontAwesomeIcon icon={faSearch}/>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
